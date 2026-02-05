79d598877df113ba81518a8225f6a4d168225b02
quan <11176433+quan1997@user.noreply.gitee.com>
2026-02-05 17:43:53 +0800

    1

--- gui/tabs/tab_notice.py
+++ gui/tabs/tab_notice.py
@@ -2,7 +2,7 @@ import dearpygui.dearpygui as dpg
 from theme.colors import UIColors
 
 
-def build_notice_tab():
+def build_notice_tab(blue_notice_theme):
     with dpg.tab(label=" ", tag="notice_tab", closable=False):
         dpg.add_text("更新公告：", color=UIColors.SECTION_HEADER)
         dpg.add_separator()
