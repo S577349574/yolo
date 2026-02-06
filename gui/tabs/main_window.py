c959bd473d34b33d6aff1326db023f6fa39f766d
quan <11176433+quan1997@user.noreply.gitee.com>
2026-02-06 17:22:39 +0800

    1

--- gui/tabs/main_window.py
+++ gui/tabs/main_window.py
@@ -13,10 +13,10 @@ from tabs.tab_features import build_features_tab
 
 
 def build_main_window(blue_notice_theme):
-    build_notice_tab(blue_notice_theme)
     build_system_tab(blue_notice_theme)
     build_image_source_tab(blue_notice_theme)
     build_preview_tab(blue_notice_theme)
     build_driver_keys_tab(blue_notice_theme)
     build_scripts_tab(blue_notice_theme)
     build_features_tab(blue_notice_theme)
+    build_notice_tab(blue_notice_theme)
